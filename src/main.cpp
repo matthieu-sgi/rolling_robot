dfd482e368434af42e12cb075b6cd07dbc50a570
Matthieu <matthieu2603@gmail.com>
2024-12-19 00:11:52 +0100

    add first test

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,53 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Definition of pinout
+#define RIGHT_MTR_FRWD 36
+#define RIGHT_MTR_BCKWD 37
+#define PWM_RIGHT_MTR 1
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+#define LEFT_MTR_FRWD 45
+#define LEFT_MTR_BCKWD 35
+#define LEFT_RIGHT_MTR 47
+
+#define MOTOR_STANDBY 46
+
+#define LIDAR_PWM 21
+
+void setup()
+{
+  Serial1.begin(115200);
+  Serial.begin(230400);
+
+  pinMode(RIGHT_MTR_FRWD, OUTPUT);
+  pinMode(RIGHT_MTR_BCKWD, OUTPUT);
+  pinMode(PWM_RIGHT_MTR, OUTPUT);
+
+  pinMode(LEFT_MTR_FRWD, OUTPUT);
+  pinMode(LEFT_MTR_BCKWD, OUTPUT);
+  pinMode(LEFT_RIGHT_MTR, OUTPUT);
+
+  pinMode(MOTOR_STANDBY, OUTPUT);
+  digitalWrite(MOTOR_STANDBY, 1);
+
+  pinMode(LIDAR_PWM, OUTPUT);
+  analogWrite(LIDAR_PWM, 255);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+  digitalWrite(RIGHT_MTR_FRWD, 0);
+  digitalWrite(RIGHT_MTR_BCKWD, 0);
+  digitalWrite(LEFT_MTR_FRWD, 0);
+  digitalWrite(LEFT_MTR_BCKWD, 0);
+  analogWrite(PWM_RIGHT_MTR, 100);
+  analogWrite(LEFT_RIGHT_MTR, 100);
+  delay(1000);
+  digitalWrite(RIGHT_MTR_FRWD, 0);
+  digitalWrite(RIGHT_MTR_BCKWD, 0);
+  digitalWrite(LEFT_MTR_FRWD, 0);
+  digitalWrite(LEFT_MTR_BCKWD, 0);
+  delay(1000);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // int value = Serial.read();
+  // Serial1.println(value);
 }
